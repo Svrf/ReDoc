19de95e18ca44003ed8094f66a9f2241511ab578
Roman <romanchaley@gmail.com>
2018-09-25 16:21:18 +0300

    Returned heading binding fix back

--- src/services/MarkdownRenderer.ts
+++ src/services/MarkdownRenderer.ts
@@ -35,7 +35,8 @@ export class MarkdownRenderer {
 
   constructor() {
     this.headingEnhanceRenderer = new marked.Renderer();
-    this.originalHeadingRule = this.headingEnhanceRenderer.heading;
+    this.originalHeadingRule = this.headingEnhanceRenderer.heading
+      .bind(this.headingEnhanceRenderer);
     this.headingEnhanceRenderer.heading = this.headingRule;
   }
 
